eb6b2242c2390e64ab7ebb3874ff049f8a3562db
PatharrajTech <patharrajannadurai@gmail.com>
2018-03-22 15:25:45 +0530

    Implement Base64 File Upload

--- controllers/firebaseFileUpload.js
+++ controllers/firebaseFileUpload.js
@@ -3,7 +3,6 @@ var fileEvents=function(){
     var bucket = gcs.bucket(process.env.firebaseProjectId + '.appspot.com');
 var fileUpload=function(file){
     var uuid = UUID();
-
     bucket.upload(file).then(function(err,res){
         if(!err){
             var img_path="https://firebasestorage.googleapis.com/v0/b/"+process.env.firebaseProjectId+".appspot.com/o/"+ encodeURIComponent('copyright.png')+ "?alt=media&token=" + uuid;
@@ -13,6 +12,25 @@ var fileUpload=function(file){
             return err;
         }
     });
+};
+
+
+var base64Upload=function(filePath){
+    filePath=filePath.replace('data:image/png;base64,','');
+    var file=bucket.file("new.png");
+    var buff = Buffer.from(filePath, 'binary').toString('utf-8');
+    const stream = file.createWriteStream({
+		metadata: {
+			contentType: 'image/png'
+		}
+	});
+	stream.on('error', (err) => {
+		console.log(err);
+	});
+	stream.on('finish', () => {
+		console.log("File Uploaded");
+	});
+	stream.end(new Buffer(buff, 'base64'));
 };
 
 var removeFile=function(file){
@@ -27,7 +45,8 @@ console.log(err);
 
 return {
     upload:fileUpload,
-    delete:removeFile
+    delete:removeFile,
+    base64Upload:base64Upload
 }
 
 }();
